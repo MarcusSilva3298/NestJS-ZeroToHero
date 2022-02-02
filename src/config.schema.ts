d568222491315a3b03842e61fff83a68ab58be28
MarcusSilva3298 <marcussilva3298@gmail.com>
2022-02-02 10:44:17 -0300

    Section 9 "Configuration Management" finished

---
+++ src/config.schema.ts
@@ -0,0 +1,14 @@
+import * as Joi from '@hapi/joi'
+
+export const configValidationSchema = Joi.object({
+  // DB
+  STAGE: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
+
+  // JWT
+  JWT_SECRET: Joi.string().required()
+})
